7087e35abe866420b27d1e36600f2c98943e91af
Moses <Moses@localhost>
2017-02-27 17:49:33 -0500

    Added ability to make a new bitmap tag from a dds image. Fixed bug with the value of certain enum values in the config. Bumped config version up to 2 Rearranged hotkey method enums

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ except ImportError:
 curr_dir = dirname(__file__)
 
 #               YYYY.MM.DD
-release_date = "2017.02.25"
-version = (0, 9, 8)
+release_date = "2017.02.27"
+version = (0, 9, 9)  # DONT FORGET TO UPDATE THE VERSION IN app_window.py
 
 try:
     try:
