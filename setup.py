fb3374f0ea9d03f794cb58fa008454a0c6f4cc06
Moses <29311834+MosesofEgypt@users.noreply.github.com>
2017-04-04 17:38:38 -0400

    Fixed incorrect version number in app_window.py Fixed not including tags_dir in the filepath when checking if model tag exists Allowed dependency window root to have text that is separate from its tag_path Fixed missing string replacement argument in error message Carried treating mode tags as mod2 over into the dependency frame

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ curr_dir = dirname(__file__)
 
 #               YYYY.MM.DD
 release_date = "2017.04.04"
-version = (0, 9, 15)  # DONT FORGET TO UPDATE THE VERSION IN app_window.py
+version = (0, 9, 16)  # DONT FORGET TO UPDATE THE VERSION IN app_window.py
 
 try:
     try:
