2e9452b06a08dccb310e0493d466265eaec599a9
Moses <Moses@localhost>
2017-04-09 13:28:35 -0400

    Fixed bug with mozz trying to set flags for every tag it tries to save,     even if the tag doesnt contain those flags Fixed bug where calc_internal_data was being called before determining if it could be

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ except ImportError:
 curr_dir = dirname(__file__)
 
 #               YYYY.MM.DD
-release_date = "2017.04.04"
-version = (0, 9, 16)  # DONT FORGET TO UPDATE THE VERSION IN app_window.py
+release_date = "2017.04.09"
+version = (0, 9, 17)  # DONT FORGET TO UPDATE THE VERSION IN app_window.py
 
 try:
     try:
