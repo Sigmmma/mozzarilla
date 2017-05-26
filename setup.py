4a6d5d004dd8b8add647c0d31585ef2b38b7ec42
Moses <29311834+MosesofEgypt@users.noreply.github.com>
2017-05-25 21:17:54 -0400

    Tweaked the way mozz handles fps related things.     It creates tags in whatever fps you specify in the config, but     displays them according to the flag in their header Mozz now supports shortening the tagsdir in the tag window

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ except ImportError:
 curr_dir = dirname(__file__)
 
 #               YYYY.MM.DD
-release_date = "2017.05.24"
-version = (0, 9, 24)  # DONT FORGET TO UPDATE THE VERSION IN app_window.py
+release_date = "2017.05.25"
+version = (0, 9, 25)  # DONT FORGET TO UPDATE THE VERSION IN app_window.py
 
 try:
     try:
