025c0e1ed044fd7f48a2938e3b1bf34e8e888c32
Moses_of_Egypt <Moses_of_Egypt@localhost>
2017-10-03 22:33:20 -0400

    Update to naming convention change in binilla

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ except ImportError:
 curr_dir = dirname(__file__)
 
 #               YYYY.MM.DD
-release_date = "2017.10.01"
-version = (1, 0, 6)  # DONT FORGET TO UPDATE THE VERSION IN app_window.py
+release_date = "2017.10.03"
+version = (1, 0, 7)  # DONT FORGET TO UPDATE THE VERSION IN app_window.py
 
 try:
     try:
