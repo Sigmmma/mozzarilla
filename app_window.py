86b85184c640fc8851546cd4bd581780305c500f
Moses_of_Egypt <29311834+MosesofEgypt@users.noreply.github.com>
2017-10-13 02:54:03 -0400

    Created tag specific scanning

--- app_window.py
+++ app_window.py
@@ -50,7 +50,7 @@ curr_dir = dirname(__file__)
 
 class Mozzarilla(Binilla):
     app_name = 'Mozzarilla'
-    version = '1.0.8'
+    version = '1.0.9'
     log_filename = 'mozzarilla.log'
     debug = 0
 
