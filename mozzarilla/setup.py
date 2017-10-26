c68f72fc9d717b95757075148fdd7b672e2f926e
Moses_of_Egypt <29311834+MosesofEgypt@users.noreply.github.com>
2017-10-26 19:42:10 -0400

    Bug with last_load_dir not being set properly AGAIN

--- mozzarilla/setup.py
+++ mozzarilla/setup.py
@@ -8,8 +8,8 @@ except ImportError:
 curr_dir = dirname(__file__)
 
 #               YYYY.MM.DD
-release_date = "2017.10.25"
-version = (1, 1, 0)  # DONT FORGET TO UPDATE THE VERSION IN app_window.py
+release_date = "2017.10.26"
+version = (1, 1, 1)  # DONT FORGET TO UPDATE THE VERSION IN app_window.py
 
 try:
     try:
