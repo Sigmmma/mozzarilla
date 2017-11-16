3c7f0ebd9a349564a42f32212f16806c2539840f
Moses_of_Egypt <Moses_of_Egypt@localhost>
2017-11-16 14:16:05 -0500

    Changed FieldType names in definitions to fit the new naming scheme

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ except ImportError:
 curr_dir = dirname(__file__)
 
 #               YYYY.MM.DD
-release_date = "2017.11.14"
-version = (1, 1, 4)  # DONT FORGET TO UPDATE THE VERSION IN app_window.py
+release_date = "2017.11.16"
+version = (1, 1, 5)  # DONT FORGET TO UPDATE THE VERSION IN app_window.py
 
 try:
     try:
