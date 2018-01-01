2ee6a9ec84ce949dc3ac4b7a52e11f39f3c2b51a
Moses_of_Egypt <29311834+MosesofEgypt@users.noreply.github.com>
2017-12-31 20:33:19 -0500

    Removed hek tool wrapper to make it a standalone python app Made tag scanner able to open the log after writing it

--- tools/__init__.py
+++ tools/__init__.py
@@ -8,6 +8,5 @@ from .shared_widgets import DirectoryFrame, HierarchyFrame, DependencyFrame
 from .dependency_window import DependencyWindow
 from .search_and_replace_window import SearchAndReplaceWindow
 from .tag_scanner_window import TagScannerWindow
-from .hek_tool_wrapper_window import HekToolWrapperWindow
 
 from .create_bitmap import bitmap_from_dds, bitmap_from_bitmap_source
