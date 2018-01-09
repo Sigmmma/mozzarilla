cc93205b32fff5e3350b4833530223d96d8f3a41
Moses_of_Egypt <29311834+MosesofEgypt@users.noreply.github.com>
2018-01-08 20:59:42 -0500

    Made Launch Pool button not appear if Pool is missing

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ except ImportError:
 curr_dir = dirname(__file__)
 
 #               YYYY.MM.DD
-release_date = "2018.01.01"
-version = (1, 1, 8)  # DONT FORGET TO UPDATE THE VERSION IN app_window.py
+release_date = "2018.01.08"
+version = (1, 1, 9)  # DONT FORGET TO UPDATE THE VERSION IN app_window.py
 
 try:
     try:
