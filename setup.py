48ce45a1683e0458c6758eb8deaf77866fcd3431
Moses_of_Egypt <Moses_of_Egypt@localhost>
2018-01-20 21:41:19 -0500

    Make it so hek_pool cant be launched unless on windows

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ except ImportError:
 curr_dir = dirname(__file__)
 
 #               YYYY.MM.DD
-release_date = "2018.01.08"
-version = (1, 1, 9)  # DONT FORGET TO UPDATE THE VERSION IN app_window.py
+release_date = "2018.01.20"
+version = (1, 2, 0)  # DONT FORGET TO UPDATE THE VERSION IN app_window.py
 
 try:
     try:
