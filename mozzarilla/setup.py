ed1d22549271fe81bb4d2a7310d871543c34fee9
Moses_of_Egypt <29311834+MosesofEgypt@users.noreply.github.com>
2018-06-18 04:17:57 -0400

    Added scenario open sauce scrubber Added get_handler method

--- mozzarilla/setup.py
+++ mozzarilla/setup.py
@@ -8,8 +8,8 @@ except ImportError:
 curr_dir = dirname(__file__)
 
 #               YYYY.MM.DD
-release_date = "2018.06.14"
-version = (1, 2, 6)  # DONT FORGET TO UPDATE THE VERSION IN app_window.py
+release_date = "2018.06.16"
+version = (1, 2, 7)  # DONT FORGET TO UPDATE THE VERSION IN app_window.py
 
 try:
     try:
--- mozzarilla/tools/__init__.py
+++ mozzarilla/tools/__init__.py
@@ -1,10 +1,11 @@
 
-__all__ = ("SearchAndReplaceWindow",
+__all__ = ("SearchAndReplaceWindow", "SauceRemovalWindow",
            "TagScannerWindow", "DependencyWindow",
            "DirectoryFrame", "HierarchyFrame", "DependencyFrame",
            "bitmap_from_dds", "bitmap_from_bitmap_source")
 
 from .shared_widgets import DirectoryFrame, HierarchyFrame, DependencyFrame
+from .sauce_removal_window import SauceRemovalWindow
 from .dependency_window import DependencyWindow
 from .search_and_replace_window import SearchAndReplaceWindow
 from .tag_scanner_window import TagScannerWindow
