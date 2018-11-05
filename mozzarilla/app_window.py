3e86c34b3cbe7915a52ec94520e8eb9e0f8f2165
Moses_of_Egypt <29311834+MosesofEgypt@users.noreply.github.com>
2018-11-04 19:30:31 -0500

    version bump

--- mozzarilla/app_window.py
+++ mozzarilla/app_window.py
@@ -55,7 +55,7 @@ this_curr_dir = get_cwd(__file__)
 
 class Mozzarilla(Binilla):
     app_name = 'Mozzarilla'
-    version = '1.4.0'
+    version = '1.4.1'
     log_filename = 'mozzarilla.log'
     debug = 0
 
