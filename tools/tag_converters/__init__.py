7dbb614cddbdf4ecce9ade337510ad0fa04f2e15
Moses_of_Egypt <29311834+MosesofEgypt@users.noreply.github.com>
2019-06-10 04:49:36 -0400

    Added animation converter Refactoring

--- tools/tag_converters/__init__.py
+++ tools/tag_converters/__init__.py
@@ -1,7 +1,8 @@
 __all__ = ("ModelConverter", "GbxmodelConverter", "ChicagoShaderConverter",
-           "CollisionConverter", "SbspConverter")
+           "ModelAnimationsConverter", "CollisionConverter", "SbspConverter")
 
 from .model_converter import ModelConverter
+from .model_animations_converter import ModelAnimationsConverter
 from .gbxmodel_converter import GbxmodelConverter
 from .chicago_shader_converter import ChicagoShaderConverter
 from .collision_converter import CollisionConverter
