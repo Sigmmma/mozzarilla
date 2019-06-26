95e4a56fe46eb3c638211deaee064bb799da9217
Moses_of_Egypt <29311834+MosesofEgypt@users.noreply.github.com>
2019-06-26 19:38:23 -0400

    Added animations decompressor window into mozz

--- tools/__init__.py
+++ tools/__init__.py
@@ -7,7 +7,7 @@ __all__ = (
     "bitmap_from_dds", "bitmap_from_multiple_dds", "bitmap_from_bitmap_source",
     "ModelCompilerWindow", "physics_from_jms",
     "hud_message_text_from_hmt", "strings_from_txt",
-    "AnimationsCompilerWindow", "GbxmodelConverter", "ModelConverter",
+    "AnimationsCompilerWindow", "AnimationsCompressionWindow", "GbxmodelConverter", "ModelConverter",
     "ChicagoShaderConverter", "CollisionConverter", "SbspConverter")
 
 from .shared_widgets import DirectoryFrame, HierarchyFrame, DependencyFrame
@@ -19,6 +19,7 @@ from .bitmap_source_extractor_window import BitmapSourceExtractorWindow
 from .bitmap_converter_window import BitmapConverterWindow
 from .tag_scanner_window import TagScannerWindow
 
+from .animations_compression_window import AnimationsCompressionWindow
 from .animations_compiler_window import AnimationsCompilerWindow
 from .model_compiler_window import ModelCompilerWindow
 
