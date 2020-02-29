017d0c5bc50b7b8c6810934bc6e42319b918fe0f
gbMichelle <gbmichelle.dev@gmail.com>
2020-02-29 17:43:14 +0100

    Future proof Py version check

--- mozzarilla/__main__.py
+++ mozzarilla/__main__.py
@@ -13,9 +13,9 @@ import sys
 def main():
     info = sys.version_info
 
-    if info[0] < 3:
+    if info[0] < 3 or (info[0] == 3 and info[1] < 5):
         input(
-            "You must have python 3 or higher installed to run Binilla.\n" +
+            "You must have python 3.5 or higher installed to run Mozzarilla.\n"
             "You currently have %s.%s.%s installed instead." % info[:3])
         raise SystemExit(0)
 
