4116350ccd7581ef9e1b58050049950966cc8493
gbMichelle <gbmichelle.dev@gmail.com>
2020-02-29 15:50:15 +0100

    Move run code to main py and add a main method

--- mozzarilla/run.py
+++ mozzarilla/run.py
@@ -8,35 +8,12 @@
 # See LICENSE for more information.
 #
 
-import sys
+# Legacy run module, used by older MEKs
 
-info = sys.version_info
-
-if info[0] < 3 or info[1] < 5:
-    input(
-        "You must have python 3.5 or higher installed to run Mozzarilla.\n"
-        "You currently have %s.%s.%s installed instead." % info[:3])
-    raise SystemExit(0)
-
-from datetime import datetime
-from traceback import format_exc
+from .__main__ import main
 
 try:
-    from mozzarilla.app_window import Mozzarilla
-    main_window = Mozzarilla(debug=1)
-    main_window.mainloop()
-
-except Exception:
-    exception = format_exc()
-    try:
-        main_window.log_file.write('\n' + exception)
-    except Exception:
-        try:
-            with open('startup_crash.log', 'a+') as cfile:
-                time = datetime.now().strftime("%Y-%m-%d  %H:%M:%S")
-                cfile.write("\n%s%s%s\n" % ("-"*30, time, "-"*(50-len(time))))
-                cfile.write(time + exception)
-        except Exception:
-            pass
-    print(exception)
+    main():
+    # Input was how the terminal window was kept open on Windows to show the
+    # error.
     input()
