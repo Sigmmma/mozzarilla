252f76ea03caca56d9b7d0e45e264a8e8ea4afab
gbMichelle <gbmichelle.dev@gmail.com>
2020-02-29 17:50:47 +0100

    Fix run.py

--- mozzarilla/run.py
+++ mozzarilla/run.py
@@ -11,9 +11,7 @@
 # Legacy run module, used by older MEKs
 
 from .__main__ import main
-
-try:
-    main():
+if main():
     # Input was how the terminal window was kept open on Windows to show the
     # error.
     input()
