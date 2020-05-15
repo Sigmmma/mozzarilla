80e8eb8e7f52df2e2f6709740a8297ff07976594
gbMichelle <gbmichelle.dev@gmail.com>
2020-05-15 09:36:06 +0200

    Make settings directory sensible on Windows

--- mozzarilla/__init__.py
+++ mozzarilla/__init__.py
@@ -12,6 +12,6 @@
 # ##############
 __author__ = "Sigmmma"
 #           YYYY.MM.DD
-__date__ = "2020.05.01"
-__version__ = (1, 9, 3)
+__date__ = "2020.05.15"
+__version__ = (1, 9, 4)
 __website__ = "https://github.com/Sigmmma/mozzarilla"
