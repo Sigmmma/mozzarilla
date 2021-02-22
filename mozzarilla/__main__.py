4c4613f2f56c6d1b9fc14bcbf4ec32498f63c3c9
gbMichelle <gbmichelle.dev@gmail.com>
2021-02-22 05:05:34 +0100

    Fix version check

--- mozzarilla/__main__.py
+++ mozzarilla/__main__.py
@@ -13,9 +13,9 @@ import sys
 def main():
     info = sys.version_info
 
-    if info[0] < 3 or (info[0] == 3 and info[1] < 5):
+    if info[0] < 3 or (info[0] == 3 and info[1] < 6):
         input(
-            "You must have python 3.5 or higher installed to run Mozzarilla.\n"
+            "You must have python 3.6 or higher installed to run Mozzarilla.\n"
             "You currently have %s.%s.%s installed instead." % info[:3])
         raise SystemExit(0)
 
